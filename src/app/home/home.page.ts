2282ba5e87f3a550ac4941117a89858abadbad81
claud2005 <162583107+claud2005@users.noreply.github.com>
2024-12-09 14:55:36 +0000

    .
    .

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -6,10 +6,30 @@ import { Router } from '@angular/router';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit, OnDestroy {
+  isDarkMode: boolean = false; // Propriedade para armazenar o estado do tema
+  darkModeMediaQuery: MediaQueryList;
 
 
-  constructor(private router : Router) {}
+
+  constructor(private router : Router) {
+    // Inicializa a media query para detectar o modo escuro
+    this.darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+    this.isDarkMode = this.darkModeMediaQuery.matches; // Define o estado inicial
+  }
+  ngOnInit() {
+    // Adiciona um listener para mudanças no modo escuro
+    this.darkModeMediaQuery.addEventListener('change', this.handleDarkModeChange);
+  }
+
+  ngOnDestroy() {
+    // Remove o listener ao destruir o componente
+    this.darkModeMediaQuery.removeEventListener('change', this.handleDarkModeChange);
+  }
+
+  handleDarkModeChange = (event: MediaQueryListEvent) => {
+    this.isDarkMode = event.matches; // Atualiza o estado com base na mudança
+  };
 
 
   goToLogin() {
