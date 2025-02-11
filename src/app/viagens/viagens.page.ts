f82d28759424f89d8f61698b68d3ccc512bef608
Rodrigo Lopes <162582009+limaR22@users.noreply.github.com>
2025-02-11 18:22:50 +0000

    .
    .

--- src/app/viagens/viagens.page.ts
+++ src/app/viagens/viagens.page.ts
@@ -21,6 +21,7 @@ interface Viagem {
 })
 export class ViagensPage implements OnInit {
   viagens: Viagem[] = [];
+  filteredViagens: Viagem[] = [];
   apiUrl: string = 'https://mobile-api-one.vercel.app/api/travels'; // URL da API
   name: string = 'claudiofreitas@ipvc.pt'; // Seu nome de usuário
   password: string = '!1lN(XNx'; // Sua senha
@@ -39,6 +40,7 @@ export class ViagensPage implements OnInit {
 
     try {
       this.viagens = await firstValueFrom(this.http.get<Viagem[]>(this.apiUrl, { headers }));
+      this.filteredViagens = this.viagens;
     } catch (error: any) {
       console.error('Erro ao buscar viagens:', error);
     }
@@ -53,6 +55,7 @@ export class ViagensPage implements OnInit {
     try {
       await firstValueFrom(this.http.delete(`${this.apiUrl}/${viagemId}`, { headers }));
       this.viagens = this.viagens.filter(viagem => viagem.id !== viagemId);
+      this.filteredViagens = this.viagens;
       console.log('Viagem deletada com sucesso!');
     } catch (error: any) {
       console.error('Erro ao deletar viagem:', error);
@@ -62,6 +65,24 @@ export class ViagensPage implements OnInit {
   // Função para editar uma viagem
   editViagem(viagemId: string) {
     this.navCtrl.navigateForward(`/criarviagens/${viagemId}`);
+  }
+
+  // Função para filtrar as viagens com base no texto de pesquisa
+  filterViagens(event: any) {
+    const searchTerm = event.target.value.toLowerCase();
+    if (searchTerm && searchTerm.trim() !== '') {
+      this.filteredViagens = this.viagens.filter(viagem => {
+        return (
+          viagem.prop1.toLowerCase().includes(searchTerm) ||
+          viagem.description.toLowerCase().includes(searchTerm) ||
+          viagem.type.toLowerCase().includes(searchTerm) ||
+          viagem.state.toLowerCase().includes(searchTerm) ||
+          viagem.prop2.toLowerCase().includes(searchTerm)
+        );
+      });
+    } else {
+      this.filteredViagens = this.viagens;
+    }
   }
 
   // Função para voltar à página anterior
